23349ecec0b8294546acc22560a1511a85cae018
Isaac Stumm <isaac.stumm@students.snow.edu>
2026-04-22 15:34:09 -0600

    Added a DateTime demo

---
+++ DateTimeDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What is the name of your account");
+        string response = Console.ReadLine().Trim();
+        Account account = new Account(response);
+        account.Display();
+    }
+}
+public class Account
+{
+    public string Name { get; set; }
+    public DateTime Creation;
+    public Guid Id;
+
+    public Account(string name)
+    {
+        this.Name = name; 
+        Creation = DateTime.Now;
+        Id = Guid.NewGuid();
+    }
+    public void Display()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Date created: {Creation}");
+        Console.WriteLine($"Id: {Id}");
+    }
+}
